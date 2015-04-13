cc5a5c04b15a4bdaa7003e58c5c245b39e9a2b96
AndersonBrantes <andersonbrantes@gmail.com>
2015-04-12 23:26:15 -0300

    adicionando arquivo base de view do layout

---
+++ views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<header>
+		<a href="/php_mvc_blog">Home</a>
+	</header>
+
+	<?php require_once('routes.php'); ?>
+	
+</body>
+</html>
