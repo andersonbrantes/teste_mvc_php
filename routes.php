c4e51b8a6fb84fbc005a5bcd85b15597d07187d9
AndersonBrantes <andersonbrantes@gmail.com>
2015-04-12 23:50:20 -0300

    checando se controller e action estão permitidos e redirect em caso de erro

--- routes.php
+++ routes.php
@@ -11,4 +11,16 @@
 
 		$controller->{ $action }();
 	}
+
+	$controllers = array('pages' => ['home', 'error']);
+
+	if (array_key_exists($controller, $controllers)) {
+		if (in_array($action, $controllers[$controller])) {
+			call($controller, $action);
+		} else {
+			call('pages', 'error');
+		}
+	} else {
+		call('pages', 'error');
+	}
 ?>
