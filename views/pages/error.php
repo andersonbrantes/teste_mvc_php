9db183108a9867f0ac5881a2d1770c81d83520d5
AndersonBrantes <andersonbrantes@gmail.com>
2015-04-13 22:27:33 -0300

    adicionando views da home e de erro

---
+++ views/pages/error.php
@@ -0,0 +1,4 @@
+
+<p>Ops! Esta é uma página de erro</p>
+
+<p>Alguma coisa errada aconteceu.</p>
---
+++ views/pages/home.php
@@ -0,0 +1,4 @@
+
+<p>Olá <?= $first_name . ' ' . $last_name ?></p>
+
+<p>Vocë acessou a página home com sucesso!</p>
